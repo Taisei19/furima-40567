82f7ea48c4f1954c95e06ca8caa637cd7a331160
Taisei19 <cherrychan0602@gmail.com>
2024-06-15 16:30:07 +0900

    0615-3（バリデーション）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, confirmation: true
+  validates :nickname, presence: true
+  validates :familyname_kanji, presence: true
+  validates :firstname_kanji, presence: true
+  validates :familyname_kana, presence: true
+  validates :firstname_kana, presence: true
+  validates :birthday, presence: true
+  
 end
