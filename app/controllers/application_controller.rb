d84b9ab85b39d12e7e8439d35223e9040488f98f
Taisei19 <cherrychan0602@gmail.com>
2024-06-16 17:22:55 +0900

    0616-1（新規登録）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
 
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nicknamem, :familyname_kanji, :firstname_kanji, :familyname_kana, :firstname_kana, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nicknamem, :familyname_kanji, :firstname_kanji, :familyname_kana, :firstname_kana, :birthday])
+  end
+
+
 
 
   private
