ac424464422215ca3b4dab28cf715fa91fc3cf25
Taisei19 <cherrychan0602@gmail.com>
2024-06-20 21:24:33 +0900

    0620-4（修正3）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :nickname, presence: true
-  validates :familyname_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/  }
-  validates :firstname_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/  }
+  validates :familyname_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :firstname_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :familyname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
