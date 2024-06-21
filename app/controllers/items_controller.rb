8ba88784a2ddeed6f21ff89bf5f0ec5847476e60
Taisei19 <cherrychan0602@gmail.com>
2024-06-21 22:47:57 +0900

    0621-4（出品・保存）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,20 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
    
+  end
+
+  def new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, alert: "商品の出品に失敗しました。"
+    end
   end
 
   private
