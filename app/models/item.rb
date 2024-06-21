addc5994d1d8d84a9d397ff9ba471e7857a85b36
Taisei19 <cherrychan0602@gmail.com>
2024-06-21 16:28:50 +0900

    0621-3（ActiveHash・バリデーション）

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :payer
+  belongs_to :prefecture
+  belongs_to :period
+
+
+  validates :name, :explanation, :price, presence: true
+
+  validates :category_id,   numericality: { other_than: 1, message: "can't be blank" }
+  validates :state_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :payer_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :period_id,     numericality: { other_than: 1, message: "can't be blank" }
+
 end
---
+++ db/migrate/20240621060429_create_payers.rb
@@ -0,0 +1,8 @@
+class CreatePayers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :payers do |t|
+
+      t.timestamps
+    end
+  end
+end
