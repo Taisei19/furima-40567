7ee7a70ee7e7166610767b252fa8346a061d92dc
Taisei19 <cherrychan0602@gmail.com>
2024-07-05 18:10:24 +0900

    0705-1（テストコード）

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
-
 RSpec.describe Record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+     
+    before do
+      @user = FactoryBot.create(:user)
+      @item = FactoryBot.create(:item, user: @user)
+      @record = FactoryBot.build(:record, user: @user, item: @item)
+      @buyer = FactoryBot.build(:buyer, record: @record)
+    end
+
+
+  it "全ての欄に記入されている" do
+    expect(@record).to be_valid
+  end
+
+  it "郵便番号なしでは購入できない" do
+    @buyer.post_code = ""
+    @buyer.valid?
+    expect(@buyer.errors.full_messages).to include("Post code can't be blank")
+  end
+
+  it 'Prefecture_idが1では登録できない' do
+    @buyer.prefecture_id = 1
+    @buyer.valid?
+    expect(@buyer.errors.full_messages).to include("Prefecture can't be blank")
+  end
+  
+  it "住所なしでは購入できない" do
+    @buyer.city = ""
+    @buyer.valid?
+    expect(@buyer.errors.full_messages).to include("City can't be blank")
+  end
+
+  it "番地なしでは購入できない" do
+    @buyer.address = ""
+    @buyer.valid?
+    expect(@buyer.errors.full_messages).to include("Address can't be blank")
+  end
+  
+  it "電話番号なしでは購入できない" do
+    @buyer.phone_number = ""
+    @buyer.valid?
+    expect(@buyer.errors.full_messages).to include("Phone number can't be blank")
+  end
 end
